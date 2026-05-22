e24c81cd13d2fe6ca99a3e6419b176545ee53938
Sarhhh <2967529244@qq.com>
2026-05-22 13:20:07 +0800

    new

--- 计算二叉树深度.cpp
+++ 计算二叉树深度.cpp
@@ -9,6 +9,49 @@ struct TreeNode
     TreeNode *right;
 };
 
+int Depth(TreeNode *root) // 层序遍历法
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int depth = 0;
+    vector<TreeNode *> queue;
+    queue.push_back(root);
+    while (!queue.empty())
+    {
+        depth++;
+        vector<TreeNode *> tmp;
+        while (!queue.empty())
+        {
+            TreeNode *node = queue.front();
+            queue.erase(queue.begin());
+            if (node->left != NULL)
+            {
+                tmp.push_back(node->left);
+            }
+            if (node->right != NULL)
+            {
+                tmp.push_back(node->right);
+            }
+        }
+        queue = tmp;
+    }
+    return depth;
+}
+
+int DepthRecursive(TreeNode *root) // 递归法
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return max(DepthRecursive(root->left), DepthRecursive(root->right)) + 1;
+    }
+}
+
 int main()
 {
     TreeNode n1, n2, n3, n4, n5, n6, n7, n8, n9;
@@ -39,4 +82,7 @@ int main()
     n9.val = 9;
     n9.left = NULL;
     n9.right = NULL;
+
+    cout << "层序遍历计算出树的深度是：" << Depth(&n1) << endl;
+    cout << "递归遍历计算出树的深度是：" << DepthRecursive(&n1)  << endl;
 }
